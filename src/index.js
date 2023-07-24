4542ec928a8ac2566dce26e2284298298c312c48
EryckBarreto <eryck.barreto@hotmail.com>
2023-07-24 17:44:49 -0300

    aula sobre parâmetros e rotas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const app = express();
+const professores = [
+    {id: 1, nome: "Guido", stack: "Backend"},
+    {id: 2, nome: "Dani", stack: "Frontend"},
+    {id: 3, nome: "Diego", stack: "Frontend"},
+    {id: 4, nome: "Vidal", stack: "Backend"}
+];
 
-app.get('/', (requisicao, resposta) => {
-    console.log(requisicao);
-    resposta.send("Página Inicial");
-})
+// localhost:3000/professores
+app.get('/professores', (requisicao, resposta) => {
+    resposta.send(professores);
+});
+// localhost:3000/professores/1
+app.get('/professores/:id', (requisicao, resposta) => {
+    const professorEncontrado = professores.find((professor) => {
+        return professor.id === Number(requisicao.params.id);
+    });
+    resposta.send(professorEncontrado);
+});
 
 app.listen(3000);
